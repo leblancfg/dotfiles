bdf1b6523ef6fd661594aed234346431c1da3a49
François Leblanc <francois.leblanc@shopify.com>
2021-06-01 11:19:19 -0400

    clipboard script

---
+++ scripts/clipboard_md.py
@@ -0,0 +1,21 @@
+import sys
+
+import clipboard
+import pandas as pd
+
+
+def tsv_clipboard():
+    try:
+        df = pd.read_clipboard()
+        text = df.to_markdown(index=False)
+        clipboard.copy(text)
+        return text
+    except:  # noqa
+        print('😞 No dice. Make sure your clipboard contains readable data.')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    text = tsv_clipboard()
+    print('✨ Done! ✨\n\n---\n')
+    print(text)
