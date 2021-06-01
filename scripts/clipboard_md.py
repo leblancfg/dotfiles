82d46a5e529cc6d2a80c310c9fc8f4b56668d6cd
François Leblanc <francois.leblanc@shopify.com>
2021-06-01 11:22:36 -0400

    autoupdate

--- scripts/clipboard_md.py
+++ scripts/clipboard_md.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 
 def tsv_clipboard():
-    # try:
-    df = pd.read_clipboard()
-    text = df.to_markdown(index=False)
-    clipboard.copy(text)
-    return text
-    # except:  # noqa
-    #     print('😞 No dice. Make sure your clipboard contains readable data.')
-    #     sys.exit(1)
+    try:
+        df = pd.read_clipboard()
+        text = df.to_markdown(index=False)
+        clipboard.copy(text)
+        return text
+    except:  # noqa
+        print('😞 No dice. Make sure your clipboard contains readable data.')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
