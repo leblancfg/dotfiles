a3dc1ad5138f354797b8e6d18cb5b8d95707d56b
François Leblanc <francois.leblanc@shopify.com>
2024-02-23 16:32:10 -0500

    autoupdate

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,12 +1,17 @@
 vim.cmd [[
   highlight Normal guifg=NONE guibg=NONE
-  highlight Comment ctermfg=3
-  highlight Constant ctermfg=1
+  highlight Bold gui=bold
+  highlight Boolean ctermfg=6
+  highlight Comment ctermfg=9
+  highlight Constant ctermfg=10
+  highlight Number ctermfg=11
   highlight Identifier ctermfg=4
-  highlight Statement ctermfg=6
-  highlight PreProc ctermfg=5
-  highlight Type ctermfg=2
-  highlight Special ctermfg=5
+  highlight Statement ctermfg=5
+  highlight PreProc ctermfg=11
+  highlight TSStringRegex ctermfg=6
+  highlight TSConstantBuiltin ctermfg=3
+  highlight Type ctermfg=11
+  highlight Special ctermfg=3
   highlight Underlined cterm=underline
   highlight MatchParen ctermfg=4 ctermbg=2
   highlight VertSplit guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE
